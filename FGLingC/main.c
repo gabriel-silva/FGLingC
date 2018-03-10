42428758b41b6e2097cf5eb48579589e55ddcc09
Gabriel <gabriel-silva_2011@hotmail.com>
2018-03-10 17:19:11 -0300

    Initial Commit

---
+++ FGLingC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FGLingC
+//
+//  Created by Gabriel Neves on 10/03/2018.
+//  Copyright © 2018 Gabriel Neves. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
