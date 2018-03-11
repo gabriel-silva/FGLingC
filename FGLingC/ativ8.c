5262d77d46fe75756a642dafda41a7e57e9f2f28
Gabriel <gabriel-silva_2011@hotmail.com>
2018-03-10 22:22:08 -0300

    ativ8

--- FGLingC/ativ8.c
+++ FGLingC/ativ8.c
@@ -1,16 +1,17 @@
 /*
- ativ7.c
+ ativ8.c
  FGLingC
  
  Created by Gabriel Neves on 10/03/2018.
  Copyright © 2018 Gabriel Neves. All rights reserved.
  
- Receber um nome e imprimir as 4 primeiras letras do nome.
+ Receber um nome e imprimir as letras na posição impar.
  
  */
 
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
     
@@ -19,12 +20,15 @@ int main(int argc, const char * argv[]) {
     
     printf("Insira um nome: ");
     fgets(nome, 20, stdin);
-    for(i = 0; i < 4; i++){
-        printf("%c", nome[i]);
+    for(i = 0; i < strlen(nome)-1; i++){
+        if(i % 2 == 1){
+            printf("%c", nome[i]);
+        }
     }
     printf("\n");
     
     return 0;
     
 }
+
 
